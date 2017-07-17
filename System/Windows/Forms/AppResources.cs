ded6858feef60aa90cad8ba16096a77335b07a01
Meragon <dragon33@bk.ru>
2017-07-17 08:46:09 +0300

    fixed resources (Unity #Case 919256)

--- System/Windows/Forms/AppResources.cs
+++ System/Windows/Forms/AppResources.cs
@@ -1,93 +1,92 @@
-﻿namespace System.Windows.Forms
+﻿
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Image = UnityEngine.Texture2D;
+using uFont = UnityEngine.Font;
+
+[Serializable]
+public class AppResources
 {
-    using System.Collections.Generic;
+    public List<uFont> Fonts;
 
-    using UnityEngine;
+    public ReservedResources Images;
 
-    using Image = UnityEngine.Texture2D;
-    using uFont = UnityEngine.Font;
+    /// <summary>
+    /// System resources.
+    /// </summary>
+    [Serializable]
+    public struct ReservedResources
+    {
+        [Tooltip("Form resize icon")]
+        public Image ArrowDown;
+
+        [Tooltip("Form resize icon, MonthCalendar, TabControl")]
+        public Image ArrowLeft;
+
+        [Tooltip("Form resize icon, MonthCalendar, TabControl")]
+        public Image ArrowRight;
+
+        [Tooltip("Form resize icon")]
+        public Image ArrowUp;
+
+        public Image Circle;
+
+        [Tooltip("Checkbox, ToolStripItem")]
+        public Image Checked;
+
+        [Tooltip("Form close button")]
+        public Image Close;
+
+        public CursorImages Cursors;
+
+        [Tooltip("ComboBox, VScrollBar")]
+        public Image CurvedArrowDown;
+
+        [Tooltip("HScrollBar")]
+        public Image CurvedArrowLeft;
+
+        [Tooltip("HScrollBar")]
+        public Image CurvedArrowRight;
+
+        [Tooltip("VScrollBar")]
+        public Image CurvedArrowUp;
+
+        [Tooltip("ToolStripDropDown")]
+        public Image DropDownRightArrow;
+
+        [Tooltip("Form")]
+        public Image FormResize;
+
+        [Tooltip("NumericUpDown")]
+        public Image NumericDown;
+
+        [Tooltip("NumericUpDown")]
+        public Image NumericUp;
+
+        [Tooltip("Tree")]
+        public Image TreeNodeCollapsed;
+
+        [Tooltip("Tree")]
+        public Image TreeNodeExpanded;
+    }
 
     [Serializable]
-    public class AppResources
+    public struct CursorImages
     {
-        public List<uFont> Fonts;
+        [Tooltip("Leave this field empty if you don't want to use your own cursor.")]
+        public Image Default;
 
-        public ReservedResources Images;
-
-        /// <summary>
-        /// System resources.
-        /// </summary>
-        [Serializable]
-        public struct ReservedResources
-        {
-            [Tooltip("Form resize icon")]
-            public Image ArrowDown;
-
-            [Tooltip("Form resize icon, MonthCalendar, TabControl")]
-            public Image ArrowLeft;
-
-            [Tooltip("Form resize icon, MonthCalendar, TabControl")]
-            public Image ArrowRight;
-
-            [Tooltip("Form resize icon")]
-            public Image ArrowUp;
-
-            public Image Circle;
-
-            [Tooltip("Checkbox, ToolStripItem")]
-            public Image Checked;
-
-            [Tooltip("Form close button")]
-            public Image Close;
-
-            public CursorImages Cursors;
-
-            [Tooltip("ComboBox, VScrollBar")]
-            public Image CurvedArrowDown;
-
-            [Tooltip("HScrollBar")]
-            public Image CurvedArrowLeft;
-
-            [Tooltip("HScrollBar")]
-            public Image CurvedArrowRight;
-
-            [Tooltip("VScrollBar")]
-            public Image CurvedArrowUp;
-
-            [Tooltip("ToolStripDropDown")]
-            public Image DropDownRightArrow;
-
-            [Tooltip("Form")]
-            public Image FormResize;
-
-            [Tooltip("NumericUpDown")]
-            public Image NumericDown;
-
-            [Tooltip("NumericUpDown")]
-            public Image NumericUp;
-
-            [Tooltip("Tree")]
-            public Image TreeNodeCollapsed;
-
-            [Tooltip("Tree")]
-            public Image TreeNodeExpanded;
-        }
-
-        [Serializable]
-        public struct CursorImages
-        {
-            [Tooltip("Leave this field empty if you don't want to use your own cursor.")]
-            public Image Default;
-
-            public Image Hand;
-            public Image Help;
-            public Image HSplit;
-            public Image SizeAll;
-            public Image SizeNESW;
-            public Image SizeNS;
-            public Image SizeNWSE;
-            public Image SizeWE;
-            public Image VSplit;
-        }
+        public Image Hand;
+        public Image Help;
+        public Image HSplit;
+        public Image SizeAll;
+        public Image SizeNESW;
+        public Image SizeNS;
+        public Image SizeNWSE;
+        public Image SizeWE;
+        public Image VSplit;
     }
 }
